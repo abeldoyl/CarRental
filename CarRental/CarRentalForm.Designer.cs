c7cb79e618d6aec5b8f73e44b1585ab3713208e1
abeldoyl <abeldoyle71@gmail.com>
2026-04-18 14:06:30 -0600

    added basic format for form

--- CarRental/CarRentalForm.Designer.cs
+++ CarRental/CarRentalForm.Designer.cs
@@ -28,12 +28,426 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            CustomerNameLabel = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            textBox5 = new TextBox();
+            label4 = new Label();
+            textBox6 = new TextBox();
+            label5 = new Label();
+            textBox7 = new TextBox();
+            label6 = new Label();
+            textBox8 = new TextBox();
+            label7 = new Label();
+            textBox9 = new TextBox();
+            label8 = new Label();
+            textBox10 = new TextBox();
+            label9 = new Label();
+            textBox11 = new TextBox();
+            label10 = new Label();
+            textBox12 = new TextBox();
+            label11 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // CustomerNameLabel
+            // 
+            CustomerNameLabel.AutoSize = true;
+            CustomerNameLabel.Location = new Point(12, 42);
+            CustomerNameLabel.Name = "CustomerNameLabel";
+            CustomerNameLabel.Size = new Size(94, 15);
+            CustomerNameLabel.TabIndex = 0;
+            CustomerNameLabel.Text = "Customer Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(140, 39);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Customer Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(140, 73);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(140, 141);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 144);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Customer Name";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(140, 107);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 110);
+            label3.Name = "label3";
+            label3.Size = new Size(94, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Customer Name";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(140, 210);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 11;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 213);
+            label4.Name = "label4";
+            label4.Size = new Size(94, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Customer Name";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(140, 176);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 179);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Customer Name";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(140, 244);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 23);
+            textBox7.TabIndex = 15;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 247);
+            label6.Name = "label6";
+            label6.Size = new Size(94, 15);
+            label6.TabIndex = 14;
+            label6.Text = "Customer Name";
+            // 
+            // textBox8
+            // 
+            textBox8.BackColor = Color.Gainsboro;
+            textBox8.Enabled = false;
+            textBox8.Location = new Point(606, 42);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(478, 45);
+            label7.Name = "label7";
+            label7.Size = new Size(94, 15);
+            label7.TabIndex = 12;
+            label7.Text = "Customer Name";
+            // 
+            // textBox9
+            // 
+            textBox9.BackColor = Color.Gainsboro;
+            textBox9.Enabled = false;
+            textBox9.Location = new Point(606, 110);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 19;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(478, 113);
+            label8.Name = "label8";
+            label8.Size = new Size(94, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Customer Name";
+            // 
+            // textBox10
+            // 
+            textBox10.BackColor = Color.Gainsboro;
+            textBox10.Enabled = false;
+            textBox10.Location = new Point(606, 76);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(100, 23);
+            textBox10.TabIndex = 17;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(478, 79);
+            label9.Name = "label9";
+            label9.Size = new Size(94, 15);
+            label9.TabIndex = 16;
+            label9.Text = "Customer Name";
+            // 
+            // textBox11
+            // 
+            textBox11.BackColor = Color.Gainsboro;
+            textBox11.Enabled = false;
+            textBox11.Location = new Point(606, 176);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(100, 23);
+            textBox11.TabIndex = 23;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(478, 179);
+            label10.Name = "label10";
+            label10.Size = new Size(94, 15);
+            label10.TabIndex = 22;
+            label10.Text = "Customer Name";
+            // 
+            // textBox12
+            // 
+            textBox12.BackColor = Color.Gainsboro;
+            textBox12.Enabled = false;
+            textBox12.Location = new Point(606, 142);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(100, 23);
+            textBox12.TabIndex = 21;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(478, 145);
+            label11.Name = "label11";
+            label11.Size = new Size(94, 15);
+            label11.TabIndex = 20;
+            label11.Text = "Customer Name";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(53, 29);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(94, 19);
+            radioButton1.TabIndex = 24;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(53, 54);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(94, 19);
+            radioButton2.TabIndex = 25;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(55, 34);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(82, 19);
+            checkBox1.TabIndex = 26;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(55, 59);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(82, 19);
+            checkBox2.TabIndex = 27;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(87, 290);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(284, 100);
+            groupBox1.TabIndex = 28;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(checkBox2);
+            groupBox2.Controls.Add(checkBox1);
+            groupBox2.Location = new Point(402, 290);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(285, 100);
+            groupBox2.TabIndex = 29;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(169, 396);
+            button1.Name = "button1";
+            button1.Size = new Size(90, 40);
+            button1.TabIndex = 30;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(281, 396);
+            button2.Name = "button2";
+            button2.Size = new Size(90, 40);
+            button2.TabIndex = 31;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(401, 396);
+            button3.Name = "button3";
+            button3.Size = new Size(90, 40);
+            button3.TabIndex = 32;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(525, 396);
+            button4.Name = "button4";
+            button4.Size = new Size(90, 40);
+            button4.TabIndex = 33;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // CarRentalForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(textBox11);
+            Controls.Add(label10);
+            Controls.Add(textBox12);
+            Controls.Add(label11);
+            Controls.Add(textBox9);
+            Controls.Add(label8);
+            Controls.Add(textBox10);
+            Controls.Add(label9);
+            Controls.Add(textBox7);
+            Controls.Add(label6);
+            Controls.Add(textBox8);
+            Controls.Add(label7);
+            Controls.Add(textBox5);
+            Controls.Add(label4);
+            Controls.Add(textBox6);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(textBox4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(CustomerNameLabel);
+            Name = "CarRentalForm";
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label CustomerNameLabel;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label2;
+        private TextBox textBox4;
+        private Label label3;
+        private TextBox textBox5;
+        private Label label4;
+        private TextBox textBox6;
+        private Label label5;
+        private TextBox textBox7;
+        private Label label6;
+        private TextBox textBox8;
+        private Label label7;
+        private TextBox textBox9;
+        private Label label8;
+        private TextBox textBox10;
+        private Label label9;
+        private TextBox textBox11;
+        private Label label10;
+        private TextBox textBox12;
+        private Label label11;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
